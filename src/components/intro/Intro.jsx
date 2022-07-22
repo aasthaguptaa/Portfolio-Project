741f7d7584ca98b43ecf17273dca4a649c79753e
Astha Gupta <guptaastha023@gmail.com>
2022-07-22 16:24:52 +0530

    intro page responsive changes

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,43 +1,48 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from "react";
 import { init } from "ityped";
 import { KeyboardArrowDown } from "@material-ui/icons";
 
-import "./Intro.scss"
+import "./Intro.scss";
 
 export default function Intro() {
-    const textRef = useRef();
+  const textRef = useRef();
 
-    useEffect(() => {
-        init(textRef.current, {
-            showCursor: true,
-            cursorChar: "|",
-            backDelay: 1500,
-            backSpeed: 60,
-            strings: ["Developer", "Engineer", "Designer"],
-        })
-    }, [])
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      cursorChar: "|",
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Developer", "Engineer", "Designer"],
+    });
+  }, []);
 
-    return (
-        <div className="intro" id="intro">
-            <div className="left">
-                <div className="imgContainer">
-                    <img src="assets/AsthaFinal.png" alt="Astha" />
-                </div>
-            </div>
-            <div className="right">
-            <div className="smallContainer">
-            </div>
-                <div className="wrapper">
-                    <h2> Hello! I am</h2>
-                    <h1> Astha Gupta</h1>
-                    <h3> Creative
-                        <span ref={textRef}></span>
-                    </h3>
-                </div>
-                <a href="#portfolio"> 
-                        <KeyboardArrowDown style={{fontSize: "50px", color: "#7d2323b5"}} />
-                </a>
-            </div>
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/AsthaFinal.png" alt="Astha"/>
         </div>
-    )
+      </div>
+      <div className="center">
+        <div className="wrapper">
+          <h2> Hello! I am</h2>
+          <h1> Astha Gupta</h1>
+          <h3>
+            {" "}
+            Creative
+            <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <KeyboardArrowDown style={{ fontSize: "50px", color: "#7d2323b5" }} />
+        </a>
+      </div>
+
+      <div className="right">
+        <div className="smallContainer">
+        </div>
+      </div>
+    </div>
+  );
 }
